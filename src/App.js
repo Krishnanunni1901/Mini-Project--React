c26eb4612308c939dd16bc797b3a4d91da69807d
arunreghunath96 <arunreghunath1996@gmail.com>
2021-03-26 16:02:23 +0530

    created header and table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Closed from "./components/Closed";
+import Header from "./components/Header";
+// import High from "./components/High";
+// import Low from "./components/Low";
+// import Medium from "./components/Medium";
+// import Open from "./components/Open";
+import Table from "./components/Table";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Table/>
+      
     </div>
   );
 }
